f9a0d2808a4e503bc2a9ee572967fe5e20965e8f
marcusvks <marcus.kayser@hotmail.com>
2019-08-07 19:02:44 -0300

    FOI

--- Treinamento/1 - VIEW/VIEW MENU/ViewMenuFuncionario.cs
+++ Treinamento/1 - VIEW/VIEW MENU/ViewMenuFuncionario.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Treinamento._3___DAO;
 
 namespace Treinamento._1___VIEW
 {
     public class ViewMenuFuncionario
     {
+        public void IniciaMenuFuncionario(FuncionarioDao funcionarioDao, ViewFuncionario viewFuncionario)
+        {
+            ConsoleKeyInfo _opcao;
+            ViewMenu viewMenu = new ViewMenu();
+
+            Console.WriteLine("\n PRESSIONE: \n\n F1 Para cadastrar um Funcionario \n F2 Para mostrar todos os funcionarios \n F12 para voltar ao menu principal");
+            _opcao = Console.ReadKey();
+            switch (_opcao.Key)
+            {
+                case ConsoleKey.F1:
+                    Console.Clear();
+                    viewFuncionario.CadastraFuncionario(funcionarioDao);
+                    break;
+
+                case ConsoleKey.F2:
+                    Console.Clear();
+                    viewFuncionario.FormataListaDadosFuncionario(funcionarioDao);
+                    break;
+
+                case ConsoleKey.F12:
+                    Console.Clear();
+                    viewMenu.IniciaMenu();
+                    break;
+            }
+        }
     }
 }
--- Treinamento/1 - VIEW/VIEW MENU/ViewMenuPessoa.cs
+++ Treinamento/1 - VIEW/VIEW MENU/ViewMenuPessoa.cs
@@ -8,5 +8,31 @@ namespace Treinamento._1___VIEW
 {
     public class ViewMenuPessoa
     {
+        public void StartMenuPessoa(ViewPessoa viewPessoa,PessoaDao pessoaDao)
+        {
+            ConsoleKeyInfo _opcao;
+            ViewMenu viewMenu = new ViewMenu();
+
+            Console.WriteLine("\n PRESSIONE: \n\n F1 Para cadastrar uma pessoa \n F2 Para listar todas as pessoas \n F12 para voltar ao menu principal");
+            _opcao = Console.ReadKey();
+            switch (_opcao.Key)
+            {
+                case ConsoleKey.F1:
+                    Console.Clear();
+                    viewPessoa.CadastraPessoa(pessoaDao);
+                    break;
+
+                case ConsoleKey.F2:
+                    Console.Clear();
+                    viewPessoa.FormataListaDadosPessoa(pessoaDao);
+                    break;
+
+                case ConsoleKey.F12:
+                    Console.Clear();
+                    viewMenu.IniciaMenu();
+                    break;
+            }
+        }
+
     }
 }
